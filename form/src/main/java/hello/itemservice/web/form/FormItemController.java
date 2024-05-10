da3820561c710c70a218ffdfed1d1c97b59908e0
iamminseongKim <minseongkimim@gmail.com>
2024-05-10 09:04:03 +0900

    섹션 2 - 타임리프 - 스프링 통합과 폼 - 체크 박스 - 멀티

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,16 +3,21 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
@@ -39,6 +44,8 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -56,6 +63,15 @@ public class FormItemController {
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
+    }
+
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return  regions;
     }
 
 }
