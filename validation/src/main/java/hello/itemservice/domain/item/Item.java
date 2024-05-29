d23c43882d05085e99c9d126450699d7aa152da6
iamminseongKim <minseongkimim@gmail.com>
2024-05-30 08:48:01 +0900

    스프링 MVC 2편 - 섹션 4 - 검증2 - Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,13 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000이 넘어야 합니다.")
 public class Item {
 
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "공백은 입력할 수 없습니다!!!!")
     private String itemName;
 
     @NotNull
