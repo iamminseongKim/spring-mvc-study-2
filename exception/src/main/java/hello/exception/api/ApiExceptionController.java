8332238ea05e828a5ad53eb879217033d4ec21c1
iamminseongKim <minseongkimim@gmail.com>
2024-06-25 08:50:32 +0900

    섹션 9 - API 예외처리 - API 예외 처리 - 스프링이 제공하는 ExceptionResolver 1

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,10 +1,16 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
+import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.lang.module.ResolutionException;
 
 @RestController
 public class ApiExceptionController {
@@ -19,8 +25,23 @@ public class ApiExceptionController {
             throw new IllegalArgumentException("잘못된 입력 값");
         }
 
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
         return new MemberDto(id, "hello " +id);
     }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
 
     @Data
     @AllArgsConstructor
