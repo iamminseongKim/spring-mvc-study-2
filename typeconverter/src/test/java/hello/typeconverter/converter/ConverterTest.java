cb73c8bc69bf75764832fe30d28270139af0e26b
iamminseongKim <minseongkimim@gmail.com>
2024-06-27 10:03:48 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 섹션 10 - 스프링 타입 컨버터 - 타입 컨버터 - Converter

--- typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -25,22 +25,22 @@ class ConverterTest {
         assertThat(result).isEqualTo("10");
     }
 
-//    @Test
-//    void stringToIpPort() {
-//        IpPortToStringConverter converter = new IpPortToStringConverter();
-//
-//        IpPort source = new IpPort("127.0.0.1", 8080);
-//        String result = converter.convert(source);
-//        assertThat(result).isEqualTo("127.0.0.1:8080");
-//    }
-//
-//    @Test
-//    void ipPortToString() {
-//        StringToIpPortConverter converter = new StringToIpPortConverter();
-//        String source = "127.0.0.1:8080";
-//        IpPort convert = converter.convert(source);
-//
-//        assertThat(convert).isEqualTo(new IpPort("127.0.0.1", 8080)); // @EqualsAndHashCode 때문에 비교 가능
-//
-//    }
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort convert = converter.convert(source);
+
+        assertThat(convert).isEqualTo(new IpPort("127.0.0.1", 8080)); // @EqualsAndHashCode 때문에 비교 가능
+
+    }
 }
